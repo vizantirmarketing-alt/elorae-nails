0a5be090f3b597d7484ccb8616aeb72ca877abe4
James Tram <vizantirmarketing@gmail.com>
2026-04-09 21:09:11 -0700

    remove debug console.log from GoogleAnalytics

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -3,7 +3,6 @@
 import Script from 'next/script'
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID
-console.log('GA_ID:', GA_ID)
 
 export default function GoogleAnalytics() {
   if (!GA_ID) return null
